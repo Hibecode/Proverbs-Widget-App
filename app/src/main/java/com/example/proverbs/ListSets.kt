22e99bf2b6431b0577729ac98e8da596fd217b20
hibecode <ibkokunoye@gmail.com>
2022-01-15 01:23:32 +0100

    Created function to get all verses from Proverbs

--- app/src/main/java/com/example/proverbs/ListSets.kt
+++ app/src/main/java/com/example/proverbs/ListSets.kt
@@ -1,7 +1,44 @@
 package com.example.proverbs
 
+import android.content.Context
+import android.widget.TextView
+import com.example.proverbs.model.Books
+import com.google.gson.Gson
+import java.io.IOException
 
 
-fun getListAllLists(){
+fun getListAllLists(context: Context): List<String>{
+    val jsonString = getJSONDataFromAsset(context, "Proverbs.json")
 
+    val gson = Gson()
+    val list1 = gson.fromJson(jsonString, Books::class.java)
+    val newList =  mutableListOf("")
+    for (i in list1.chapters){
+        for (j in i.verses){
+            newList.add(j.text)
+        }
+    }
+    return newList
+}
+
+/*fun working5(){
+    val jsonString = getJSONDataFromAsset(this, "Proverbs.json")
+    val gson = Gson()
+    val listt = gson.fromJson(jsonString, Books::class.java)
+    *//*var displaytext: TextView = findViewById(R.id.testTV)
+
+    displaytext.text = listt.chapters[4].verses[8].text*//*
+}*/
+
+
+fun getJSONDataFromAsset(context: Context, fileName: String): String? {
+    val jsonString: String
+    try{
+        jsonString = context.assets.open(fileName)
+            .bufferedReader().use{it.readText()}
+    } catch (e: IOException) {
+        e.printStackTrace()
+        return null
+    }
+    return jsonString
 }
